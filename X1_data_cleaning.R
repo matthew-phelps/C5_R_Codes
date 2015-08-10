96b95ce364baf69bf0a35aeab3073805d1df37cd
Matthew Phelps <mdp553@hotmail.com>
2015-08-10 15:02:03 +0200

    Nice date formats
    Make dates human readable

--- X1_data_cleaning.R
+++ X1_data_cleaning.R
@@ -13,4 +13,15 @@ library(plyr)
 library(xlsx)
 
 x1 <- read.csv("X-1 Choleraphone distribution 31Jul15.csv", sep=";")
-x1$Date.of.baseline <- as.Date(x1$Date.of.baseline, "%d.%m.%y")
+
+# remove rows without a HHID
+x1 <- x1[complete.cases(x1$HHID),]
+
+# turn date characters into date format
+x1_2 <- lapply(x1[,3:5], as.Date, "%d.%m.%y")
+
+# format dates to be more human readible
+x1_2 <- lapply(x1_2, format, "%d %b %Y")
+
+# merge back into original dataset
+x2 <- cbind(x1[1:2], x1_2[1], x1_2[2], x1_2[3])
