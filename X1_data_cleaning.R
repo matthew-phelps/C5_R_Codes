66576de33ccf2a4a9d29595570646f0fd3709b06
Matthew Phelps <mdp553@hotmail.com>
2015-08-10 15:48:00 +0200

    Calculating No. of active days
    Need to add variable to indicate if HH is currently active

--- X1_data_cleaning.R
+++ X1_data_cleaning.R
@@ -18,10 +18,31 @@ x1 <- read.csv("X-1 Choleraphone distribution 31Jul15.csv", sep=";")
 x1 <- x1[complete.cases(x1$HHID),]
 
 # turn date characters into date format
-x1_2 <- lapply(x1[,3:5], as.Date, "%d.%m.%y")
+x1_2 <- lapply(x1[,3:5], as.Date, format = "%d.%m.%y")
 
-# format dates to be more human readible
-x1_2 <- lapply(x1_2, format, "%d %b %Y")
 
-# merge back into original dataset
+# Populate the withdrawl/move date as the current date for calculation purposes.
+# Use a custom function
+CurrentDate <- function(x) {
+  if (!is.na(x)) {
+    return(x)
+  } else {
+    x <- Sys.Date()
+    return(x)
+    }
+}
+
+for (i in 1:nrow(x1)) {
+  x1_2$Date.of.withdrawl.or.move[i] <- CurrentDate(x1_2$Date.of.withdrawl.or.move[i])
+}
+
 x2 <- cbind(x1[1:2], x1_2[1], x1_2[2], x1_2[3])
+rm(x1, x1_2)
+
+
+
+# Calculate number of days each unique household is active
+x2$daysActive <- as.numeric(x2$Date.of.withdrawl.or.move - x2$Date.of.phone.distribution)
+
+
+hist(x2$daysActive, breaks = 15)
