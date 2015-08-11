743c0c7cdc5d08f31814136ca14549947cb76664
Matthew Phelps <mdp553@hotmail.com>
2015-08-11 09:39:12 +0200

    Unique IDs
    Created unique IDs by merging HHID and Listing number together

--- X1_data_cleaning.R
+++ X1_data_cleaning.R
@@ -17,9 +17,17 @@ x1 <- read.csv("X-1 Choleraphone distribution 31Jul15.csv", sep=";")
 # remove rows without a HHID
 x1 <- x1[complete.cases(x1$HHID),]
 
+
+# Format HHID and Listing number to have standard length. Then create unique ID
+x1$HHID <-formatC(x1$HHID, width = 3, format = 'd', flag = 0)
+x1$Listing.number <- formatC(x1$Listing.number, width = 4, format = 'd', flag = 0)
+
+# create unique ID called "unique_HHID_list_no"
+x1$unique_ID <- paste(x1$HHID, x1$Listing.number, sep="-")
+
+
 # turn date characters into date format
 x1_2 <- lapply(x1[,3:5], as.Date, format = "%d.%m.%y")
-
 
 # Populate the withdrawl/move date as the current date for calculation purposes.
 # Use a custom function
@@ -36,8 +44,11 @@ for (i in 1:nrow(x1)) {
   x1_2$Date.of.withdrawl.or.move[i] <- CurrentDate(x1_2$Date.of.withdrawl.or.move[i])
 }
 
-x2 <- cbind(x1[1:2], x1_2[1], x1_2[2], x1_2[3])
+# Merge to create finished dataset
+x2 <- cbind(x1[c(1:2, 6)], x1_2[1], x1_2[2], x1_2[3])
 rm(x1, x1_2)
+
+
 
 
 
