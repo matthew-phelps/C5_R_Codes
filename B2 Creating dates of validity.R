71100041652c9fb90e5ce0866c0342fe6f80961e
Matthew Phelps <mdp553@hotmail.com>
2015-08-31 12:25:10 +0200

    Moved data cleaning upstream
    Instead of changing only the unique ID of records where the date was
    incorrect, change the original date value and re-create the unique IDs.

--- B2 Creating dates of validity.R
+++ B2 Creating dates of validity.R
@@ -22,38 +22,61 @@ fileNames.df <- file.info(list.files(path = getwd(),
 fileNames.df <- fileNames.df[with(fileNames.df, order(as.POSIXct(mtime))), ]
 x1.name <- fileNames.df[nrow(fileNames.df), ] # Subset the most recently modified file
 x1.path <- rownames(x1.name) # Get file path
-x1 <- read.xlsx2(file = x1.path, sheetIndex = 1, stringsAsFactors = F) # turn into df
+x1_data <- read.xlsx2(file = x1.path, sheetIndex = 1, stringsAsFactors = F) # turn into df
 
 # Store end.date for data validation (make sure no records are after the file creation date)
 end.date <- as.Date(x1.name$mtime)
 rm(x1.name, x1.path, fileNames.df)
 
+# Only keep records with a HHID
+x1_data <- x1_data[x1_data$HHID > 0, ]
 
-x1_data <- read.csv2("X-1 Choleraphone distribution 31Jul15.csv")
-x1_data <- x1_data[which(!(is.na(x1_data$HHID))),]
-
+# Formate dates of x1
 x1_data$base_date <- as.Date(x1_data$Date.of.baseline, "%d.%m.%y")
 x1_data$with_date <- as.Date(x1_data$Date.of.withdrawl.or.move, "%d.%m.%y")
+
+# Create unique IDs for x1
 x1_data$uniqueID <- paste(x1_data$HHID, "_", x1_data$base_date, sep="")
 
-# set current_date to day data was downloaded
-current_date <- "2015-08-15"
+# Return HHID to numeric format
+x1_data$HHID <- as.numeric(x1_data$HHID)
+
+# Set current_date to day data was downloaded
+current_date <- end.date
+
+# Any records missing a withdrawl date are assigned the current date as withdrawl date
 x1_data$with_date[is.na(x1_data$with_date)] <- current_date
+
+
 x1_data$interval_check <- NA
 
-#cleaning, see which uniqueids in x1 are not in baseline
 
-sort(x1_data$uniqueID[!(x1_data$uniqueID %in% baselineAll$uniqueID)])
+# 2.) Check data: which unique IDs in x1 are not in baseline. Create df of x1 not in baseline --------------------------------------------------------------------
 
-missing20_08_2015<-("100_2014-11-25", "109_2015-05-27", "118_2015-05-31", "126_2015-06-21", "129_2015-06-24", "144_2014-10-20", "16_2014-06-03", 
-                    "168_2015-05-08", "171_2014-10-24", "186_2015-05-24", "199_2015-06-01", "214_2015-06-05", "217_2014-10-31", "220_2014-06-11",
-                    "223_2015-04-17", "225_2015-06-28", "230_2014-08-15", "235_2014-09-16", "248_2014-08-07", "251_2014-08-19", "252_2014-06-01",
-                    "253_2014-08-26", "254_2014-09-25", "257_2015-05-23", "270_2015-06-12", "28_2014-09-12",  "280_2015-06-26", "295_2014-08-18",
-                    "300_2014-11-14", "322_2014-10-27", "330_2015-06-26", "332_2014-11-14", "333_2014-07-20", "35_2015-07-24",  "388_2015-07-11",
-                    "391_2014-11-14", "50_2015-05-29",  "60_2014-10-21",  "67_2014-06-05",  "78_2015-07-04",  "83_2014-09-12",  "85_2014-06-01", 
-                    "90_2015-07-04") 
+x1.not.in.baseline <- (x1_data[!(x1_data$uniqueID %in% baselineAll$uniqueID), ])
 
+# Don't think this code is needed. Please uncomment if is needed 
+# missing20_08_2015<-c("100_2014-11-25", "109_2015-05-27", "118_2015-05-31", "126_2015-06-21", "129_2015-06-24", "144_2014-10-20", "16_2014-06-03", 
+#                     "168_2015-05-08", "171_2014-10-24", "186_2015-05-24", "199_2015-06-01", "214_2015-06-05", "217_2014-10-31", "220_2014-06-11",
+#                     "223_2015-04-17", "225_2015-06-28", "230_2014-08-15", "235_2014-09-16", "248_2014-08-07", "251_2014-08-19", "252_2014-06-01",
+#                     "253_2014-08-26", "254_2014-09-25", "257_2015-05-23", "270_2015-06-12", "28_2014-09-12",  "280_2015-06-26", "295_2014-08-18",
+#                     "300_2014-11-14", "322_2014-10-27", "330_2015-06-26", "332_2014-11-14", "333_2014-07-20", "35_2015-07-24",  "388_2015-07-11",
+#                     "391_2014-11-14", "50_2015-05-29",  "60_2014-10-21",  "67_2014-06-05",  "78_2015-07-04",  "83_2014-09-12",  "85_2014-06-01", 
+#                     "90_2015-07-04") 
 
+# Create list comparing x1 and baseline:
+hhid <- sort(x1.not.in.baseline$HHID)
+missing.ls <- vector("list", length(hhid))
+for (i in 1:length(hhid)) {
+  missing.ls[[i]]$baseUniqueID <-  (baselineAll[baselineAll$hhid == hhid[i],c("uniqueID")])
+  missing.ls[[i]]$x1UniqueID <- (x1_data[x1_data$HHID == hhid[i],c("uniqueID")])
+}
+
+missing.ls[[1]]
+print(missing.ls)
+
+# Based on the HHIDs of the unique IDs that are not in baseline, check difference
+# with x1
 baselineAll$uniqueID[baselineAll$hhid==50]
 x1_data$uniqueID[x1_data$HHID==50]
 baselineAll$uniqueID[baselineAll$hhid==60]
@@ -145,21 +168,58 @@ x1_data$uniqueID[x1_data$HHID==388]
 baselineAll$uniqueID[baselineAll$hhid==391]
 x1_data$uniqueID[x1_data$HHID==391]
 
-#change dates in X-1 that are off by a keystroke to match baseline
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="83_2014-09-12","83_2014-09-11",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="100_2014-11-25","100_2014-11-24",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="171_2014-10-14","171_2014-11-14",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="230_2014-08-15","230_2014-08-16",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="235_2014-09-16","235_2014-09-15",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="248_2014-08-07","248_2014-07-07",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="28_2014-09-12","28_2014-09-11",x1_data$uniqueID)
-x1_data$uniqueID<-ifelse(x1_data$uniqueID=="295_2014-08-18","295_2014-08-08",x1_data$uniqueID)
+
+# 3.) Perform Cleaning ----------------------------------------------------
+
+# Change dates in X-1 that are off by a keystroke to match baseline
+x1_data$base_date[x1_data$uniqueID== "083_2014-09-12"] <- "2014-09-11"
+x1_data$base_date[x1_data$uniqueID== "100_2014-11-25"] <- "2014-11-24"
+x1_data$base_date[x1_data$uniqueID== "171_2014-10-14"] <- "2014-11-14"
+x1_data$base_date[x1_data$uniqueID== "230_2014-08-15"] <- "2014-08-16"
+x1_data$base_date[x1_data$uniqueID== "235_2014-09-16"] <- "2014-09-15"
+x1_data$base_date[x1_data$uniqueID== "248_2014-08-07"] <- "2014-07-07"
+x1_data$base_date[x1_data$uniqueID== "028_2014-09-12"] <- "2014-09-11"
+x1_data$base_date[x1_data$uniqueID== "295_2014-08-18"] <- "2014-08-08"
+
+# Change dates in baseline that are incorrect, in order to match with X-1
+baselineAll$intdate[baselineAll$uniqueID == "333_2014-04-20"] <- "2014-07-20"
+
+
+# Re-create unique IDs with new dates
+baselineAll$hhid <- formatC(baselineAll$hhid, width = 3, format = 'd', flag = 0)
+baselineAll$uniqueID<-paste(baselineAll$hhid,"_",baselineAll$intdate,sep="")
+baselineAll$hhid <- as.numeric(baselineAll$hhid)
+
+x1_data$HHID <- formatC(x1_data$HHID, width = 3, format = 'd', flag = 0)
+x1_data$uniqueID<-paste(x1_data$HHID,"_",x1_data$base_date,sep="")
+x1_data$HHID <- as.numeric(x1_data$HHID)
+
+
+mon5_5$hh_id[mon5_5$visitdate=="2015-01-14"]
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="083_2014-09-12","083_2014-09-11",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="100_2014-11-25","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="171_2014-10-14","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="230_2014-08-15","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="235_2014-09-16","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="248_2014-08-07","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="028_2014-09-12","",x1_data$uniqueID)
+x1_data$uniqueID<-ifelse(x1_data$uniqueID=="295_2014-08-18","",x1_data$uniqueID)
 
 #change dates in baseline that are incorrect, in order to match with X-1
-baselineAll$uniqueID<-ifelse(baselineAll$uniqueID=="333_2014-04-20","333_2014-07-20",baselineAll$uniqueID)
 
-#Check to see which baseline entries are not in x1 
-baseline_hhid_not_in_X1<-c(sort(!baselineAll$hhid[(baselineAll$uniqueID %in% x1_data$uniqueID)]))
+
+
+
+# 3.) Repeat data check after cleaning -------------------------------
+ x1.not.in.baseline <- (x1_data[!(x1_data$uniqueID %in% baselineAll$uniqueID), ])
+
+
+
+
+baseline_hhid_not_in_X1<-(sort(baselineAll[!(baselineAll$uniqueID %in% x1_data$uniqueID), c("uniqueID", "hhid")]))
+
+
+
 sort(baseline_hhid_not_in_X1[!(baseline_hhid_not_in_X1%in% MonthlyAll$hhid)])
 #baseline_in_X1<-c(sort(baselineAll$hhid[(baselineAll$uniqueID %in% x1_data$uniqueID)]))
 # sort(baseline_in_X1[!(baseline_in_X1%in% MonthlyAll$hhid)])
