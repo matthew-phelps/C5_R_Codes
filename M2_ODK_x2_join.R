26ac8dcaa93836c9bbde53000a886a2093849688
Matthew Phelps <mdp553@hotmail.com>
2015-09-01 14:06:25 +0200

    Code almost ready for X2-ODK merge
    Now need to check which records are in ODK and not X2 - and visa-versa

--- M2_ODK_x2_join.R
+++ M2_ODK_x2_join.R
@@ -1,13 +1,15 @@
 # Author: Char Tamason & Matthew Phelps
 # Desc:    Cleaning of entire dataset using X-2 file 
 # output: Cleaned monthly visits
-# DEPENDENCIES: Requires M1, X2_data_cleaning and A1 to have been run
+# DEPENDENCIES: Requires M1 to have been run
 
 
 # Intro -------------------------------------------------------------------
 
+library(dplyr)
+library(xlsx)
 
-# Prepare Matthew's workspace if user == CHAR prepare Char's path, else: MAtthew's path
+# Prepare workspace: if user == CHAR prepare Char's path, else: MAtthew's path
 ifelse(grepl("zrc340", getwd()),
        NA,
        rm(list = ls()))
@@ -18,60 +20,96 @@ ifelse(grepl("zrc340", getwd()),
        ODK.path <- "C:/Users/zrc340/Desktop/Dropbox/C5 data/C5 Monthly Visits Data/Raw data direct from ODK",
        ODK.path <-"C:/Users/wrz741/Dropbox/C5 Monthly Visits Data/Raw data direct from ODK")
 ifelse(grepl("zrc340", getwd()),
-       data.output.path <- "CHAR - PUT PATH TO /Rdata HERE",
-       data.output.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\")
+       x2.path <- "CHAR - PUT PATH TO X2 Monthly Visit HERE",
+       x2.path <-"C:\\Users\\wrz741\\Dropbox\\C5 Field Operations data\\X-2 Monthly visit tracking sheet")
+ifelse(grepl("zrc340", getwd()),
+       odk <- "CHAR - PUT PATH TO CLEANED ODK Monthly Visit HERE",
+       odk <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\month_all.Rdata")
 
 
+# 1.) LOAD FILES ----------------------------------------------------------
 
-## What is this from? Can we stop using A1??
-x2 <- "C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\X2_cleaned.Rdata"
-x2.ct <- "PATH HERE"
-setwd(mp)
-rm(mp, ct)
+# ODK Cleaned data
+load(odk)
 
-library(dplyr)
+# X2 data
+fileNames.df <- file.info(list.files(path = x2.path,
+                                     pattern = "X-2 monthly visits.*\\.xlsx$", full.names = T))
+# Sort files that match pattern by most modification time and 
+# extract the rowname of the most recently modified file:
+fileNames.df <- fileNames.df[with(fileNames.df, order(as.POSIXct(mtime))), ]
+x2.name <- fileNames.df[nrow(fileNames.df), ] # Subset the most recently modified file
+x2.path <- rownames(x2.name) # Get file path
+x2 <- read.xlsx2(file = x2.path, sheetIndex = 1, stringsAsFactors = F) # turn into df
+
 
 
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
-# Go to this code to add or remove variables of interest
+# Store latest X-2 upload date from Bangladesh for data validation (make sure no records are after the file creation date)
+end.date <- as.Date(x2.name$mtime)
+rm(x2.name, x2.path, fileNames.df)
+start.date <- as.Date("2014-09-09")
 
 
-# 1.) LOAD DATA -----------------------------------------------------------
 
-load(baseline.path)
-load(x2)
-x2 <- a5
-rm(a5)
-
-
-# 2.) REMOVE COLUMNS ------------------------------------------------------
-
-# Re-order columns so Date visit fields are first.
-
-
+# 2.) Format data to make easy to work with ------------------------------------------------------
 
 # Subset monthlyAll to make it easier to work with
 m <- MonthlyAll[, c('visitdate', 'hh_id', "FRA")]
 
-# make x-2 an integet so ani_join can work later on
+# make HHID inteter
 x2$HHID <- as.integer(x2$HHID)
 
+# Remove empyt records
+x2 <- x2[!is.na(x2$HHID), ]
+
+
+# x2 dates
+x2$date_visit <- as.Date(x2$Date.of.monthly.visit, format = "%d.%m.%y")
+
+# clean columns for simplicity
+x2$Date.of.monthly.visit <- NULL
+x2$ppl <- x2$Numer.of.ppl.in.household.at.monthly.visit
+x2$Numer.of.ppl.in.household.at.monthly.visit <- NULL
+
+x2 <- x2[order(x2$HHID, x2$date_visit), ]
+row.names(x2) <- NULL
+
+
+
+# Check X2 for date range -------------------------------------------------
+
+date_wrong <- x2[x2$date_visit > end.date | x2$date_visit < start.date, ]
+rm(date_wrong)
 
 # 3.) CHECK DUPLICATE RECORDS ---------------------------------------------
 
-# Find observations where HH was recorded as being visited twice on same date:
-x <- (duplicated(MonthlyAll[, c('visitdate', 'hh_id')]))
+# Check ODK for where HH was recorded as being visited twice on same date:
+x <- (duplicated(m[, c('visitdate', 'hh_id')]))
 y <- which(x %in% T) #Gives index of duplicates
-duplicates.df <- MonthlyAll[c(y, y-1), ] # gives df of duplicates. y-1 makes sure we get the
+duplicates.odk <- m[c(y, y-1), ] # gives df of duplicates. y-1 makes sure we get the
 # 'original' and the 'duplicate'
-rm(x, y)
+
+# Remove duplicates as needed based on info from BD -----------------------
+# Drop HHID 185 duplicate
+# Need to delete row 237 (?) or 109 (?) depending on word from Bangladesh
+droprows <- y[2]
+MonthlyAll <- MonthlyAll[-c(droprows), ]
+rm(x, y, duplicates.odk)
 
 
-# Remove hhid 185 (row 41)
-MonthlyAll = MonthlyAll[-41,]
-#need to delete row 237 or 109 depending on word from Bangladesh
+
+# Check X2 for where HH was recorded as being visited twice on same date:
+x <- (duplicated(x2[, c('date_visit', 'HHID')]))
+y <- which(x %in% T) #Gives index of duplicates
+duplicates.x2 <- x2[c(y, y-1), ] # gives df of duplicates. y-1 makes sure we get the
+# 'original' and the 'duplicate'
+rm(x, y, duplicates.x2)
+
+
+
 
 
 
