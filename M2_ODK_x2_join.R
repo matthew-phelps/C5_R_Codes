06d50e5602022999b590efac78b50ab390d3527d
Matthew Phelps <mdp553@hotmail.com>
2015-09-02 11:32:34 +0200

    ODK delay accounted for
    ODK records are not updated as quickly as X2 (so it seems - need to
    check why). So only keeping records that occur <= max ODK date

--- M2_ODK_x2_join.R
+++ M2_ODK_x2_join.R
@@ -1,5 +1,5 @@
 # Author: Char Tamason & Matthew Phelps
-# Desc:    Cleaning of entire dataset using X-2 file 
+# Desc:    Merge ODK and X-2 files
 # output: Cleaned monthly visits
 # DEPENDENCIES: Requires M1 to have been run
 
@@ -14,17 +14,17 @@ ifelse(grepl("zrc340", getwd()),
        NA,
        rm(list = ls()))
 ifelse(grepl("zrc340", getwd()),
-       baseline.path <- "CHAR - PUT PATH TO /Rdata HERE",
-       baseline.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\baseline_x1_merge.Rdata")
-ifelse(grepl("zrc340", getwd()),
-       ODK.path <- "C:/Users/zrc340/Desktop/Dropbox/C5 data/C5 Monthly Visits Data/Raw data direct from ODK",
-       ODK.path <-"C:/Users/wrz741/Dropbox/C5 Monthly Visits Data/Raw data direct from ODK")
-ifelse(grepl("zrc340", getwd()),
-       x2.path <- "CHAR - PUT PATH TO X2 Monthly Visit HERE",
+       x2.path <- "C:/Users/zrc340/Desktop/Dropbox/C5 data/C5 Field Operations data/X-2 Monthly visit tracking sheet",
        x2.path <-"C:\\Users\\wrz741\\Dropbox\\C5 Field Operations data\\X-2 Monthly visit tracking sheet")
 ifelse(grepl("zrc340", getwd()),
-       odk <- "CHAR - PUT PATH TO CLEANED ODK Monthly Visit HERE",
+       odk <- "CHAR - PUT PATH TO CLEANED ODK Monthly Visit (M1 Output) HERE",
        odk <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\month_all.Rdata")
+ifelse(grepl("zrc340", getwd()),
+       data.output.path <- "CHAR - PUT PATH TO RDATA HERE",
+       data.output.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\monthly-odk-x2-joined.Rdata")
+ifelse(grepl("zrc340", getwd()),
+       monthly.enddate.path <- "CHAR - PUT PATH TO RDATA HERE",
+       monthly.enddate.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\monthly-end-date.Rdata")
 
 
 # 1.) LOAD FILES ----------------------------------------------------------
@@ -32,7 +32,7 @@ ifelse(grepl("zrc340", getwd()),
 # ODK Cleaned data
 load(odk)
 
-# X2 data
+# Most recent X2 data
 fileNames.df <- file.info(list.files(path = x2.path,
                                      pattern = "X-2 monthly visits.*\\.xlsx$", full.names = T))
 # Sort files that match pattern by most modification time and 
@@ -48,16 +48,19 @@ x2 <- read.xlsx2(file = x2.path, sheetIndex = 1, stringsAsFactors = F) # turn in
 # GLOBAL VARIABLES --------------------------------------------------------
 
 # Store latest X-2 upload date from Bangladesh for data validation (make sure no records are after the file creation date)
-end.date <- as.Date(x2.name$mtime)
+end.date.x2 <- as.Date(x2.name$mtime)
 rm(x2.name, x2.path, fileNames.df)
 start.date <- as.Date("2014-09-09")
 
+odk.end.date <- max(MonthlyAll$visitdate)
 
 
 # 2.) Format data to make easy to work with ------------------------------------------------------
 
-# Subset monthlyAll to make it easier to work with
-mz <- MonthlyAll[, c('visitdate', 'hh_id', "FRA")]
+# x2 dates
+x2$date_visit <- as.Date(x2$Date.of.monthly.visit, format = "%d.%m.%y")
+x2 <- x2[order(x2$HHID, x2$date_visit), ]
+row.names(x2) <- NULL
 
 # make HHID inteter
 x2$HHID <- as.integer(x2$HHID)
@@ -65,20 +68,16 @@ x2$HHID <- as.integer(x2$HHID)
 # Remove empyt records
 x2 <- x2[!is.na(x2$HHID), ]
 
-# x2 dates
-x2$date_visit <- as.Date(x2$Date.of.monthly.visit, format = "%d.%m.%y")
-
 # clean columns for simplicity
 x2$Date.of.monthly.visit <- NULL
 x2$ppl <- x2$Numer.of.ppl.in.household.at.monthly.visit
 x2$Numer.of.ppl.in.household.at.monthly.visit <- NULL
 
-x2 <- x2[order(x2$HHID, x2$date_visit), ]
-row.names(x2) <- NULL
+
 
 # Check X2 for date range -------------------------------------------------
 
-date_wrong <- x2[x2$date_visit > end.date | x2$date_visit < start.date, ]
+date_wrong <- x2[x2$date_visit > end.date.x2 | x2$date_visit < start.date, ]
 rm(date_wrong)
 
 
@@ -87,9 +86,9 @@ rm(date_wrong)
 # 3.) CHECK DUPLICATE RECORDS ---------------------------------------------
 
 # Check ODK for where HH was recorded as being visited twice on same date:
-x <- (duplicated(mz[, c('visitdate', 'hh_id')]))
+x <- (duplicated(MonthlyAll[, c('visitdate', 'hh_id')]))
 y <- which(x %in% T) #Gives index of duplicates
-duplicates.odk <- mz[c(y, y-1), ] # gives df of duplicates. y-1 makes sure we get the
+duplicates.odk <- MonthlyAll[c(y, y-1), c('hh_id', 'visitdate', 'FRA')] # gives df of duplicates. y-1 makes sure we get the
 # 'original' and the 'duplicate'
 
 # Remove duplicates as needed based on info from BD:
@@ -97,7 +96,6 @@ duplicates.odk <- mz[c(y, y-1), ] # gives df of duplicates. y-1 makes sure we ge
 # Need to delete row 237 (?) or 109 (?) depending on word from Bangladesh
 droprows <- y[2]
 MonthlyAll <- MonthlyAll[-c(droprows), ]
-mz <- MonthlyAll[, c('visitdate', 'hh_id', "FRA")]
 rm(x, y, duplicates.odk)
 
 # Check X2 for where HH was recorded as being visited twice on same date:
@@ -163,21 +161,51 @@ rm(mz, temp.merge, z, x)
 
 # Check missing records ---------------------------------------------------
 
-not.in.x2 <- temp2[is.na(temp2$Listing.number), ]
+not.in.x2 <- temp2[is.na(temp2$Listing.number), c("HHID", 'date_visit', 'FRA', 'Listing.number')]
 not.in.x2 <- not.in.x2[order(not.in.x2$HHID, not.in.x2$date_visit), ]
 row.names(not.in.x2) <- NULL
 
-not.in.odk <- temp2[is.na(temp2$FRA), c(1:4)]
+not.in.odk <- temp2[is.na(temp2$FRA), c("HHID", 'date_visit', 'ppl', "Listing.number")]
 not.in.odk <- not.in.odk[order(not.in.odk$HHID, not.in.odk$date_visit), ]
 row.names(not.in.odk) <- NULL
 
 
 
 
+#  CLEAN based on Bangladesh response---------------------------------------
 
 
 
-######## END - below is not functional ################################
+# RE-CHECK DATA AFTER CLEANING --------------------------------------------
+
+# Check duplicates
+z <- duplicated(temp2[, 1:3])
+sum(z)
+
+# Check missing records
+not.in.x2 <- temp2[is.na(temp2$Listing.number), c("HHID", 'date_visit', 'FRA', 'Listing.number')]
+not.in.x2 <- not.in.x2[order(not.in.x2$HHID, not.in.x2$date_visit), ]
+row.names(not.in.x2) <- NULL
+
+not.in.odk <- temp2[is.na(temp2$FRA), c("HHID", 'date_visit', 'ppl', "Listing.number")]
+not.in.odk <- not.in.odk[order(not.in.odk$HHID, not.in.odk$date_visit), ]
+row.names(not.in.odk) <- NULL
+
+# Account for delay in ODK reporting
+not.in.odk <- not.in.odk[not.in.odk$date_visit <= odk.end.date, ]
+
+month_visit <- temp2[temp2$date_visit <= odk.end.date, ]
+
+
+#  SAVE MERGRED ODK-X2 FILE-----------------------------------------------------
+save(month_visit, file = data.output.path)
+save(end.date.x2, file = )
+
+
+
+
+######## END - code below is not functional ################################
+######## 
 # 4.) COMBINE TO X-2 ------------------------------------------------------
 
 hhCleanup <- function(x) {
