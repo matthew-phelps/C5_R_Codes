ab4539d682b8fee19cd08af7be0fbbdb172acadd
Matthew Phelps <mdp553@hotmail.com>
2015-09-11 09:30:34 +0200

    Updates to monthly cleaning
    Added lines to clean some records

--- M2_ODK_x2_join.R
+++ M2_ODK_x2_join.R
@@ -9,9 +9,7 @@ library(dplyr)
 library(xlsx)
 
 # Prepare workspace: if user == CHAR prepare Char's path, else: MAtthew's path
-ifelse(grepl("zrc340", getwd()),
-       NA,
-       rm(list = ls()) + NA)
+rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
        x2.path <- "C:/Users/zrc340/Desktop/Dropbox/C5 data/C5 Field Operations data/X-2 Monthly visit tracking sheet",
        x2.path <-"C:\\Users\\wrz741\\Dropbox\\C5 Field Operations data\\X-2 Monthly visit tracking sheet")
@@ -21,11 +19,21 @@ ifelse(grepl("zrc340", getwd()),
 ifelse(grepl("zrc340", getwd()),
        data.output.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\Rdata\\monthly-odk-x2-joined.Rdata",
        data.output.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\monthly-odk-x2-joined.Rdata")
-
-
+ifelse(grepl("zrc340", getwd()),
+       functions.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\c_5_functions_source_file.R",
+       functions.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\c_5_functions_source_file.R")
+ifelse(grepl("zrc340", getwd()),
+       not.in.odk.csv.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\Rdata\\missing_from_ODK.csv",
+       not.in.odk.csv.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\missing_from_ODK.csv")
+ifelse(grepl("zrc340", getwd()),
+       not.in.X2.csv.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\Rdata\\missing_from_X-2.csv",
+       not.in.X2.csv.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\missing_from_X-2.csv")
 
 # 1.) LOAD FILES ----------------------------------------------------------
 
+# Functions:
+source(functions.path)
+ 
 # ODK Cleaned data
 load(odk)
 
@@ -72,11 +80,13 @@ x2$Numer.of.ppl.in.household.at.monthly.visit <- NULL
 
 
 
-# Check X2 for date range -------------------------------------------------
+# RESTRICT DATE RANGE -------------------------------------------------
 
 date_wrong <- x2[x2$date_visit > end.date.x2 | x2$date_visit < start.date, ]
+ifelse(nrow(date_wrong)==0, rm(date_wrong) + NA, NA)
 rm(date_wrong)
 
+x2 <- x2[x2$date_visit < odk.end.date, ]
 
 
 
@@ -108,49 +118,15 @@ rm(x, y, duplicates.x2)
 # Check for typos in entry ------------------------------------------------
 
 # We assume that if there is a 1 - 2 day differene between ODK and X2, we use
-# X2 dates CHANGE - NEED TO USE ODK
-temp.merge <- merge(x2, MonthlyAll, by.x = c('HHID', 'date_visit'),  by.y = c('hh_id', 'visitdate') ,  all = T)
+# ODK dates
+temp.x2.odk.merge <- merge(x2, MonthlyAll, by.x = c('HHID', 'date_visit'),  by.y = c('hh_id', 'visitdate') ,  all = T)
 
-x <- split(temp.merge, temp.merge$HHID)
+x <- split(temp.x2.odk.merge, temp.x2.odk.merge$HHID)
 
 # Apply myFun1 to each element in the list formed by the "split" above:
-dateReplace <- function (x) {
-  # On each row, test to see if there is a ODK entry, and the entry before or after was only a X2 entry
-  # If row i is odk and row i+1 or i-1 is only x2 - take the date from row i and apply it to row i+1 or i-1
-  for (i in 1:nrow(x)){
-    
-    if(i> 1 && i < nrow(x) && is.na(x[i, ]$Listing.number) && (is.na(x[i-1, ]$FRA) | is.na(x[i+1, ]$FRA))){
-      if(x[i, ]$date_visit <= (x[i-1, ]$date_visit + 2)) {
-        x[i-1, ]$date_visit <- x[i, ]$date_visit
-      } else if (x[i, ]$date_visit >= (x[i+1, ]$date_visit - 2)) {
-        x[i+1, ]$date_visit <- x[i, ]$date_visit
-      }
-    } else if(i == 1 && nrow(x) >1 && is.na(x[i, ]$Listing.number) && is.na(x[i+1, ]$FRA) && x[i, ]$date_visit >= (x[i+1, ]$date_visit - 2)) {
-      x[i+1, ]$date_visit <- x[i, ]$date_visit 
-      
-    } else if (i == nrow(x) && nrow(x) > 0 && is.na(x[i, ]$Listing.number) && is.na(x[i-1, ]$FRA) && x[i, ]$date_visit <= (x[i-1, ]$date_visit + 2)) {
-      x[i-1, ]$date_visit <- x[i, ]$date_visit
-    }
-  }
-  return (x)
-}
-
 system.time({x <- lapply(x, dateReplace)})
 
 # Combine all data from duplicate rows into one row, then delete superfluous rows
-
-mergeRows <- function(x) {
-  for (i in 1:nrow(x)){
-    if(i < (nrow(x)) && !is.na(x[i, ]$FRA) && (x[i, ]$date_visit == x[i + 1, ]$date_visit)) {
-      x[i, ]$ppl <- x[i + 1, ]$ppl
-      x[i, ]$Listing.number <- x[i + 1, ]$Listing.number
-    } else if (i > 1 && !is.na(x[i, ]$FRA) && x[i, ]$date_visit == x[i - 1, ]$date_visit) {
-      x[i, ]$ppl <- x[i - 1, ]$ppl
-      x[i, ]$Listing.number <- x[i - 1, ]$Listing.number
-    }
-  }
-  return (x)  
-}
 system.time({x <- lapply(x, mergeRows)})
 
 # Convert back to df
@@ -160,7 +136,7 @@ temp2 <- do.call(rbind.data.frame, x)
 z <- duplicated(temp2[, 1:3])
 sum(z)
 temp2 <- temp2[z == F,  ]
-rm(temp.merge, z, x)
+rm(temp.x2.odk.merge, z, x)
 
 
 # Check missing records ---------------------------------------------------
@@ -176,40 +152,82 @@ row.names(not.in.odk) <- NULL
 
 
 
-#  CLEAN based on Bangladesh response---------------------------------------
+#  CLEAN based on logical assumptions---------------------------------------
+# If dates are off by exactly 1 month, use ODK dates
+#  If dates are off by a couple of day (+/- 3 days), use ODK dates
+
+# Changes to ODK
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-26' & MonthlyAll$hh_id == 9] <- '2014-12-26'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-20' & MonthlyAll$hh_id == 28] <- '2014-11-18'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2015-06-08' & MonthlyAll$hh_id == 24] <- '2015-07-08'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2015-06-08' & MonthlyAll$hh_id == 30] <- '2015-07-08'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2015-06-07' & MonthlyAll$hh_id == 357] <- '2015-07-07'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-19' & MonthlyAll$hh_id == 170] <- '2014-10-14'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-14' & MonthlyAll$hh_id == 249] <- '2014-10-24'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-11-04' & MonthlyAll$hh_id == 313] <- '2014-11-07'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-11-04' & MonthlyAll$hh_id == 326] <- '2014-11-07'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-11-04' & MonthlyAll$hh_id == 353] <- '2014-11-07'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-11-04' & MonthlyAll$hh_id == 102] <- '2014-11-07'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-16' & MonthlyAll$hh_id == 367] <- '2014-10-17'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-17' & MonthlyAll$hh_id == 119] <- '2014-11-17'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2015-05-09' & MonthlyAll$hh_id == 286] <- '2015-06-09'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-24' & MonthlyAll$hh_id == 240] <- '2014-10-28'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-24' & MonthlyAll$hh_id == 282] <- '2014-10-28'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-24' & MonthlyAll$hh_id == 336] <- '2014-10-28'
+MonthlyAll$visitdate[MonthlyAll$visitdate == '2014-10-23' & MonthlyAll$hh_id == 35] <- '2014-10-27'
+
+# Deletions to ODK
+MonthlyAll <- MonthlyAll[!(MonthlyAll$visitdate == '2014-09-15' & MonthlyAll$hh_id == 156), ]
 
 
+# Changes to X2
+x2$date_visit[x2$date_visit == '2015-04-09' & x2$HHID == 1] <- '2015-03-09'
+x2$date_visit[x2$date_visit == '2015-04-06' & x2$HHID == 21] <- '2015-04-03'
+x2$date_visit[x2$date_visit == '2015-04-06' & x2$HHID == 22] <- '2015-04-03'
+x2$date_visit[x2$date_visit == '2015-04-06' & x2$HHID == 40] <- '2015-04-03'
+x2$date_visit[x2$date_visit == '2015-04-06' & x2$HHID == 63] <- '2015-04-03'
+x2$date_visit[x2$date_visit == '2015-04-06' & x2$HHID == 77] <- '2015-04-03'
+x2$date_visit[x2$date_visit == '2014-09-11' & x2$HHID == 232] <- '2014-09-15'
+x2$date_visit[x2$date_visit == '2014-10-20' & x2$HHID == 367] <- '2014-10-17'
+x2$date_visit[x2$date_visit == '2015-04-13' & x2$HHID == 240] <- '2015-04-10'
+
+# Deletions to X2
+x2$date_visit[x2$date_visit == '2014-09-15' & x2$HHID == 156]
 
 # RE-CHECK DATA AFTER CLEANING --------------------------------------------
 
+temp.x3.odk.merge <- merge(x2, MonthlyAll, by.x = c('HHID', 'date_visit'),  by.y = c('hh_id', 'visitdate') ,  all = T)
+x <- split(temp.x3.odk.merge, temp.x3.odk.merge$HHID)
+
+x <- lapply(x, dateReplace)
+x <- lapply(x, mergeRows)
+temp3 <- do.call(rbind.data.frame, x)
+
+
 # Check duplicates
-z <- duplicated(temp2[, 1:3])
+z <- duplicated(temp3[, 1:3])
 sum(z)
+temp3 <- temp3[z == F,  ]
+rm(temp.x3.odk.merge, z, x)
 
 # Check missing records
-not.in.x2 <- temp2[is.na(temp2$Listing.number), c("HHID", 'date_visit', 'FRA', 'Listing.number')]
+not.in.x2 <- temp3[is.na(temp3$Listing.number), c("HHID", 'date_visit', 'FRA', 'Listing.number')]
 not.in.x2 <- not.in.x2[order(not.in.x2$HHID, not.in.x2$date_visit), ]
 row.names(not.in.x2) <- NULL
 
-not.in.odk <- temp2[is.na(temp2$FRA), c("HHID", 'date_visit', 'ppl', "Listing.number")]
+not.in.odk <- temp3[is.na(temp3$FRA), c("HHID", 'date_visit', 'ppl', "Listing.number")]
 not.in.odk <- not.in.odk[order(not.in.odk$HHID, not.in.odk$date_visit), ]
 row.names(not.in.odk) <- NULL
 
-# Account for delay in ODK reporting
-not.in.odk <- not.in.odk[not.in.odk$date_visit <= odk.end.date, ]
 
-visits.month <- temp2[temp2$date_visit <= odk.end.date, ]
+# WRITE TO CSV FOR BANGLADESH ---------------------------------------------
 
+write.csv(not.in.odk, file = not.in.odk.csv.path, row.names = F)
+write.csv(not.in.x2, file = not.in.X2.csv.path, row.names = F)
 
 #  SAVE MERGRED ODK-X2 FILE-----------------------------------------------------
 save(visits.month, file = data.output.path)
 
 
-
-
-
-######## END - code below is not functional ################################
-######## 
-# 4.) COMBINE TO X-2 ------------------------------------------------------
 
 
