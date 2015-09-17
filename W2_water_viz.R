5695187f2bc107ddf6233969c87d08f5b970ec2c
Matthew Phelps <mdp553@hotmail.com>
2015-09-17 13:47:07 +0200

    Prelim plots

--- W2_water_viz.R
+++ W2_water_viz.R
@@ -14,4 +14,24 @@ ifelse(grepl("zrc340", getwd()),
 
 load(water.usage.path)
 
+
+
+
+# PLOTS -----------------------------------------------------------------
+
+boxplot(daily_h2o_percapita~month, data=monthly)
+
+boxplot(daily_h2o_percapita ~ month, data = monthly[monthly$water_access_group == 1, ])
+boxplot(daily_h2o_percapita ~ month, data = monthly[monthly$water_access_group == 2, ])
+boxplot(daily_h2o_percapita ~ month, data = monthly[monthly$water_access_group == 3, ])
+
+
+
+
+
+
+
 monthly$daily_h2o_percapita[1]
+monthly$water_access_group2
+
+box
