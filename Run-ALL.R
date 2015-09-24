18726b9644a44fb145fcc526e73b4d5f5a2aa101
cctamason <cctamason@gmail.com>
2015-09-24 19:09:29 +0200

    changed my path
    after recloning C5 folder

--- Run-ALL.R
+++ Run-ALL.R
@@ -7,7 +7,7 @@
 # Baseline Files ----------------------------------------------------------
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -16,7 +16,7 @@ source('B1 Joint baseline file.R')
 
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -27,7 +27,7 @@ source('B2 Baseline cleaning.R')
 # Monthly files -----------------------------------------------------------
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -36,7 +36,7 @@ source('M1_ODK_cleaning.R')
 
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -44,7 +44,7 @@ source('M2_ODK_x2_join.R')
 
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -52,7 +52,7 @@ source('M3_monthly-baseline-join.R')
 
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
@@ -61,7 +61,7 @@ source('M4_cleaning-monthly-baseline.R')
 
 rm(list = ls())
 ifelse(grepl("zrc340", getwd()),
-       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes",
+       wd.path <- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\C5_R_Codes",
        wd.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes")
 
 setwd(wd.path)
