2d3f55ec8253d6d09bce7e89c4bdcb80889a8a0d
Matthew Phelps <mdp553@hotmail.com>
2015-10-01 13:58:21 +0200

    No. of new phones per month
    Working. Next - number of active people per month, number of active
    households per month

--- A5_monthly_summaries.R
+++ A5_monthly_summaries.R
@@ -1,5 +1,5 @@
 # Author: Matthew Phelps
-# Desc:    Monthly summaries. Record for each month:
+# Desc:    Monthly summaries for Leela. Record for each month:
 #   * how many actiev HHs
 #   * how many new phones distributed
 #   * number of active participants
@@ -9,8 +9,10 @@
 
 
 # Intro -------------------------------------------------------------------
+
 library(dplyr)
-library(data.table)
+library(lubridate)
+#detach("package:plyr", unload=TRUE) # disrupts the dplyr package
 
 # Prepare workspace: if user == CHAR prepare Char's path, else: MAtthew's path
 rm(list = ls())
@@ -23,4 +25,35 @@ ifelse(grepl("zrc340", getwd()),
        functions.path <-"C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\c_5_functions_source_file.R")
 
 
-#detach("package:plyr", unload=TRUE) # disrupts the dplyr package
+
+
+# LOAD DATA ---------------------------------------------------------------
+
+load(pt)
+source(functions.path)
+
+
+# DATA PREPERATION -----------------------------------------------------
+
+m5$VistMonthYear <- floor_date(m5$date_visit, unit = "month")
+m5$phoneMonthYear <- floor_date(m5$phone.dist, unit = "month")
+m5$withMonthYear <- floor_date(m5$with_date, unit = "month")
+
+uniqueHH <- m5 %>%
+  group_by(uniqueID) %>%
+  summarise(
+    active_ppl = mean(ppl)
+  )
+
+uniqueHH <- merge(uniqueHH, m5, by = "uniqueID")
+x <- duplicated(uniqueHH[, 1])
+uniqueHH <- uniqueHH[!x,]
+
+# Remove variables that don't make sense for the aggregated uniqueHH dataframe:
+uniqueHH$ppl <- uniqueHH$pt <- uniqueHH$date_visit <- uniqueHH$HHID <- NULL
+
+# NEW PHONES Per MONTH ----------------------------------------------------------
+# Counting how many phones were distributed each month
+
+newPhones <- as.data.frame(table(uniqueHH$phoneMonthYear))
+
