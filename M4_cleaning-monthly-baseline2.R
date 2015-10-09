83cba9b19973179e50ec3d710b185dd541e7aafc
cctamason <cctamason@gmail.com>
2015-10-09 15:53:51 -0400

    Water use values cleaned
    as per Bimal's corrections
    
    table writing was also put into comments so they are not remade every
    time

--- M4_cleaning-monthly-baseline2.R
+++ M4_cleaning-monthly-baseline2.R
@@ -6,7 +6,7 @@
 
 
 # Intro -------------------------------------------------------------------
-
+library(chron)
 
 # Prepare workspace: if user == CHAR prepare Char's path, else: MAtthew's path
 rm(list = ls())
@@ -42,11 +42,12 @@ m4<-m4[!(m4$uniqueID=="400_2014-07-15"&m4$FRA==0),]
 
 
 #methods for identifying mislabeled HHIDs in monthly visits
-#View(m4[is.na(m4$ppl)|is.na(m4$FRA),]) # look at unmatched visits
-#View(m4[m4$date_visit=="2015-01-14",]) #Look at date of unmatched monthly visit to see if there is a corresponding X-2 date with a different ID
+View(m4[is.na(m4$ppl)|is.na(m4$FRA),]) # look at unmatched visits
+View(m4[m4$date_visit=="2014-11-18",]) #Look at date of unmatched monthly visit to see if there is a corresponding X-2 date with a different ID
 #View(m4[(m4$uniqueID=="327_2014-07-12"|m4$uniqueID=="372_2014-12-05"),]) #if yes, view both IDs and compare containers
 #View(m4[m4$uniqueID=="395_2014-07-17",]) #if a HHID has a missing X-2 and monthly visit, check dates
-
+View(m4[m4$cont4.cont4_size==15&m4$cont1.cont1_size==2.5,])
+m4<-m4[!(is.na(m4$uniqueID)),]
 
 # Clean water quantities --------------------------------------------------
 
@@ -98,10 +99,10 @@ names(range_list) <- c("uniqueID", "range_min", "range_max", "range_diff")
 m4 <- merge(m4, range_list, by="uniqueID")
 
 #write table of data where water use = 0
-setwd("C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\Sent to bangladesh for clarification")
-sub<-m4[which(!(is.na(m4$FRA))),]
+#setwd("C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\Sent to bangladesh for clarification")
+#sub<-m4[which(!(is.na(m4$FRA))),]
 
-write.csv2(sub[sub$daily_h2o_percapita==0,c("HHID", "date_visit", "FRA","ppl",
+#write.csv2(sub[sub$daily_h2o_percapita==0,c("HHID", "date_visit", "FRA","ppl",
                                             "daily_volume","daily_h2o_percapita")],file="No water use.csv")
 
 #delete the three entries with no info on water use
@@ -116,13 +117,13 @@ sub<-m4[!(m4$uniqueID=="005_2014-09-12"|m4$uniqueID=="008_2014-12-09"|m4$uniqueI
             m4$uniqueID=="369_2014-07-11"|m4$uniqueID=="388_2014-11-20"|m4$uniqueID=="396_2014-07-12"),]    
     
 sub<-sub[!(is.na(sub$FRA)),]        
-write.csv2(sub[(sub$range_diff>100),c("uniqueID","listing","date_visit","FRA","ppl","daily_h2o_percapita",
-                   "cont1.cont1_size","cont1.cont1_times",	"cont2.cont2_size",	"cont2.cont2_times",	
-                   "cont3.cont3_size",	"cont3.cont3_times","cont4.cont4_size",	"cont4.cont4_times",		
-                   "cont5.cont5_size","cont5.cont5_times",	"cont6.cont6_size",	"cont6.cont6_times",
-                   "cont7.cont7_size",	"cont7.cont7_times","cont8.cont8_size",	"cont8.cont8_times",	"cont9.cont9_size",
-                   "cont9.cont9_times",	"cont10.cont10_size",	"cont10.cont10_times","other_water_in.adult_bathe_in",
-                   "other_water_out.adult_bathe_out","other_water_in.child_bathe_in","other_water_out.child_bathe_out")],file="Range over 100.csv")
+#write.csv2(sub[(sub$range_diff>100),c("uniqueID","listing","date_visit","FRA","ppl","daily_h2o_percapita",
+#                    "cont1.cont1_size","cont1.cont1_times",	"cont2.cont2_size",	"cont2.cont2_times",	
+#                    "cont3.cont3_size",	"cont3.cont3_times","cont4.cont4_size",	"cont4.cont4_times",		
+#                    "cont5.cont5_size","cont5.cont5_times",	"cont6.cont6_size",	"cont6.cont6_times",
+#                    "cont7.cont7_size",	"cont7.cont7_times","cont8.cont8_size",	"cont8.cont8_times",	"cont9.cont9_size",
+#                    "cont9.cont9_times",	"cont10.cont10_size",	"cont10.cont10_times","other_water_in.adult_bathe_in",
+#                    "other_water_out.adult_bathe_out","other_water_in.child_bathe_in","other_water_out.child_bathe_out")],file="Range over 100.csv")
 
 #verified visually, difference is because of a high or low wash day for uniqueIDs: 005_2014_09_12, 008_2014_12_09,
     #010_2014_06_02, 025_2014_09_17, 083_2014_09_11, 094_2014-09-12, 108_2014-08-22, 110_2014-10-23, 131_2014-08-29,
@@ -134,17 +135,26 @@ write.csv2(sub[(sub$range_diff>100),c("uniqueID","listing","date_visit","FRA","p
 #View(m4[m4$cont1.cont1_size==6&m4$cont2.cont2_size==2&m4$cont3.cont3_size==2,])
  #       &m4$cont2.cont2_size==22&m4$cont3.cont3_size==12&m4$cont4.cont4_size==14,])
 
-#Erase rows that have container information that doesn't match up anywhere
-m4<-m4[!(m4$uniqueID=="001_2014-09-14"&m4$date_visit==16-11-2014),]
-m4<-m4[!(m4$uniqueID=="028_2014-09-11"&m4$date_visit==18-11-2014),]
-
-
 
 m4$cont3.cont3_times[m4$cont3.cont3_times==20&m4$uniqueID=="138_2014-07-21"]<-2 #keystroke error, should be 2, not 20
 m4$cont3.cont3_times[m4$cont3.cont3_times==8&m4$uniqueID=="344_2014-11-10"]<-18 #keystroke error, all other container 3 are 18 liters
 m4$cont7.cont7_size[m4$cont7.cont7_size==212&m4$uniqueID=="219_2014-08-04"]<-21 #keystroke error, only entry missing a 21 liter container
 
 m4$ppl[m4$ppl==5&m4$uniqueID=="156_2014-08-06"]<-1 #all water use data is same but people is 5 instead of 1
+
+#correct errors checked by Bimal in water use
+
+m4$cont1.cont1_size[m4$uniqueID=="052_2014-09-05"&m4$date_visit=="2014-12-01"]<-23
+m4$date_visit[m4$uniqueID=="052_2014-09-05"&m4$date_visit=="2014-12-01"]<-as.Date("2014-11-18")
+m4$cont2.cont2_size[m4$uniqueID=="067_2014-06-05"&m4$date_visit=="2014-09-24"]<-24
+m4$cont1.cont1_times[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-4
+m4$cont2.cont2_size[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-2
+m4$cont2.cont2_times[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-3
+m4$cont3.cont3_times[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-2
+m4$cont3.cont3_times[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-2
+m4$other_water_in.adult_bathe_in[m4$uniqueID=="156_2014-08-06"&m4$date_visit=="2014-09-16"]<-0
+m4$cont2.cont2_times[m4$uniqueID=="257_2014-09-28"&m4$date_visit=="2014-12-23"]<-4
+
 
 #Check data on activities done without a container
 m4$bath_pc<-with(m4,(other_water_in.adult_bathe_in+other_water_out.adult_bathe_out+other_water_in.child_bathe_in+other_water_out.child_bathe_out)/m4$ppl)
@@ -274,72 +284,72 @@ else  if    { (length(unique(m4_subset$water_point1.wa_pt1_usebefore))==3 &
 
 # Clean hours per day that water is flowing from source -------------------
 
-monthly$water_flow1_end1<- times(monthly$water_point1.wa_flow1.wa_time1.aE)
-monthly$water_flow1_start1<-times(monthly$water_point1.wa_flow1.wa_time1.aS)
-monthly$water_flow2_end2<- times(monthly$water_point1.wa_flow1.wa_time1.bE)
-monthly$water_flow2_start2<-times(monthly$water_point1.wa_flow1.wa_time1.bS)
-monthly$water_flow3_end3<- times(monthly$water_point1.wa_flow1.wa_time1.cE)
-monthly$water_flow3_start3<-times(monthly$water_point1.wa_flow1.wa_time1.cS)
+m4$water_flow1_end1<- times(m4$water_point1.wa_flow1.wa_time1.aE)
+m4$water_flow1_start1<-times(m4$water_point1.wa_flow1.wa_time1.aS)
+m4$water_flow2_end2<- times(m4$water_point1.wa_flow1.wa_time1.bE)
+m4$water_flow2_start2<-times(m4$water_point1.wa_flow1.wa_time1.bS)
+m4$water_flow3_end3<- times(m4$water_point1.wa_flow1.wa_time1.cE)
+m4$water_flow3_start3<-times(m4$water_point1.wa_flow1.wa_time1.cS)
 
 
-monthly$water_flow_1<- monthly$water_flow1_end1-monthly$water_flow1_start1
-monthly$water_flow_1<-monthly$water_flow_1*24
-monthly$water_flow_1<-ifelse((monthly$water_flow1_end1==times("17:59:00")&monthly$water_flow1_start1==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings and daylight savings time
-                             |(monthly$water_flow1_end1==times("15:59:00")&monthly$water_flow1_start1==times("16:00:00")),24,
-                             monthly$water_flow_1)
+m4$water_flow_1<- m4$water_flow1_end1-m4$water_flow1_start1
+m4$water_flow_1<-m4$water_flow_1*24
+m4$water_flow_1<-ifelse((m4$water_flow1_end1==times("17:59:00")&m4$water_flow1_start1==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings and daylight savings time
+                             |(m4$water_flow1_end1==times("15:59:00")&m4$water_flow1_start1==times("16:00:00")),24,
+                             m4$water_flow_1)
 
-monthly$water_flow_1<-ifelse(monthly$water_flow_1<0,monthly$water_flow_1+24,monthly$water_flow_1)
+m4$water_flow_1<-ifelse(m4$water_flow_1<0,m4$water_flow_1+24,m4$water_flow_1)
 
 
-monthly$water_flow_2<- monthly$water_flow2_end2-monthly$water_flow2_start2
-monthly$water_flow_2<-monthly$water_flow_2*24
-monthly$water_flow_2<-ifelse((monthly$water_flow2_end2==times("17:59:00")&monthly$water_flow2_start2==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings
-                             |(monthly$water_flow2_end2==times("15:59:00")&monthly$water_flow2_start2==times("16:00:00")),24,
-                             monthly$water_flow_2)
-monthly$water_flow_2<-ifelse(monthly$water_flow_2<0,monthly$water_flow_2+24,monthly$water_flow_2)
+m4$water_flow_2<- m4$water_flow2_end2-m4$water_flow2_start2
+m4$water_flow_2<-m4$water_flow_2*24
+m4$water_flow_2<-ifelse((m4$water_flow2_end2==times("17:59:00")&m4$water_flow2_start2==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings
+                             |(m4$water_flow2_end2==times("15:59:00")&m4$water_flow2_start2==times("16:00:00")),24,
+                             m4$water_flow_2)
+m4$water_flow_2<-ifelse(m4$water_flow_2<0,m4$water_flow_2+24,m4$water_flow_2)
 
-monthly$water_flow_3<- monthly$water_flow3_end3-monthly$water_flow3_start3
-monthly$water_flow_3<-monthly$water_flow_3*24
-monthly$water_flow_3<-ifelse((monthly$water_flow3_end3==times("17:59:00")&monthly$water_flow3_start3==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings 
-                             |(monthly$water_flow3_end3==times("15:59:00")&monthly$water_flow3_start3==times("16:00:00")),24,
-                             monthly$water_flow_3)
-monthly$water_flow_3<-ifelse(monthly$water_flow_3<0,monthly$water_flow_3+24,monthly$water_flow_3)
+m4$water_flow_3<- m4$water_flow3_end3-m4$water_flow3_start3
+m4$water_flow_3<-m4$water_flow_3*24
+m4$water_flow_3<-ifelse((m4$water_flow3_end3==times("17:59:00")&m4$water_flow3_start3==times("18:00:00")) # time ending in 1 minute less than start time indicates 24 hours. is different because of tablet settings 
+                             |(m4$water_flow3_end3==times("15:59:00")&m4$water_flow3_start3==times("16:00:00")),24,
+                             m4$water_flow_3)
+m4$water_flow_3<-ifelse(m4$water_flow_3<0,m4$water_flow_3+24,m4$water_flow_3)
 
-monthly$round<-round(monthly$water_flow_1,3) #need to round decimal points or replacement doesn't work
-monthly$round2<-round(monthly$water_flow_2,3)
-monthly$round3<-round(monthly$water_flow_3,3)
+m4$round<-round(m4$water_flow_1,3) #need to round decimal points or replacement doesn't work
+m4$round2<-round(m4$water_flow_2,3)
+m4$round3<-round(m4$water_flow_3,3)
 
 
 
 #correct obvious typos, data collectors were instructed to round to 15 minute intervals when applicable
-monthly$water_flow_1[which(monthly$round==23.600)]<-0
-monthly$water_flow_1[which(monthly$round==23.933)]<-24
-monthly$water_flow_1[which(monthly$round==23.483)]<-24
-monthly$water_flow_1[which(monthly$round==23.150)]<-24
-monthly$water_flow_1[which(monthly$round==23.850)]<-24
-monthly$water_flow_1[which(monthly$round==23.967)]<-24
-monthly$water_flow_1[which(monthly$round==23.483)]<-24
-monthly$water_flow_1[which(monthly$round==15.983)]<-24
-monthly$water_flow_1[which(monthly$round==13.983)]<-24
+m4$water_flow_1[which(m4$round==23.600)]<-0
+m4$water_flow_1[which(m4$round==23.933)]<-24
+m4$water_flow_1[which(m4$round==23.483)]<-24
+m4$water_flow_1[which(m4$round==23.150)]<-24
+m4$water_flow_1[which(m4$round==23.850)]<-24
+m4$water_flow_1[which(m4$round==23.967)]<-24
+m4$water_flow_1[which(m4$round==23.483)]<-24
+m4$water_flow_1[which(m4$round==15.983)]<-24
+m4$water_flow_1[which(m4$round==13.983)]<-24
 
-monthly$water_flow_2[which(monthly$round>23.9)]<-0
-monthly$water_flow_3[which(monthly$round>23.9)]<-0
+m4$water_flow_2[which(m4$round>23.9)]<-0
+m4$water_flow_3[which(m4$round>23.9)]<-0
 
-# View(monthly[monthly$water_flow_1>23&monthly$water_flow_1<24,c("water_flow_1","round",
+# View(m4[m4$water_flow_1>23&m4$water_flow_1<24,c("water_flow_1","round",
 #                       "water_point1.wa_flow1.wa_time1.aS","water_point1.wa_flow1.wa_time1.aE",
 #                       "water_point1.wa_flow1.wa_time1.bS","water_point1.wa_flow1.wa_time1.bE")])
 
-monthly$checkwater<-monthly$water_flow_1+monthly$water_flow_2+monthly$water_flow_3
+m4$checkwater<-m4$water_flow_1+m4$water_flow_2+m4$water_flow_3
 
-# write.csv2(monthly[monthly$checkwater>24,c("uniqueID","Listing.number.x","date_visit",
+# write.csv2(m4[m4$checkwater>24,c("uniqueID","Listing.number.x","date_visit",
 #"water_point1.wa_flow1.wa_time1.aS","water_point1.wa_flow1.wa_time1.aE","water_flow_1",
 #"water_point1.wa_flow1.wa_time1.bS","water_point1.wa_flow1.wa_time1.bE","water_flow_2",
 #"water_point1.wa_flow1.wa_time1.cS","water_point1.wa_flow1.wa_time1.cE","water_flow_3","checkwater")], file="too many hours of water use.csv")
 
-write.csv2(monthly[monthly$round2==0.017|monthly$round==0.667|monthly$round==16.517,c("uniqueID","FRA","Listing.number.x","date_visit",
-                                                                                      "water_point1.wa_flow1.wa_time1.aS","water_point1.wa_flow1.wa_time1.aE","water_flow_1",
-                                                                                      "water_point1.wa_flow1.wa_time1.bS","water_point1.wa_flow1.wa_time1.bE","water_flow_2",
-                                                                                      "water_point1.wa_flow1.wa_time1.cS","water_point1.wa_flow1.wa_time1.cE","water_flow_3","checkwater")], file="water flow recheck.csv")
+#write.csv2(m4[m4$round2==0.017|m4$round==0.667|m4$round==16.517,c("uniqueID","FRA","Listing.number.x","date_visit",
+#                                                                                       "water_point1.wa_flow1.wa_time1.aS","water_point1.wa_flow1.wa_time1.aE","water_flow_1",
+#                                                                                       "water_point1.wa_flow1.wa_time1.bS","water_point1.wa_flow1.wa_time1.bE","water_flow_2",
+#                                                                                       "water_point1.wa_flow1.wa_time1.cS","water_point1.wa_flow1.wa_time1.cE","water_flow_3","checkwater")], file="water flow recheck.csv")
 
 
 # RE-CHECK CLEAN DATA -----------------------------------------------------
