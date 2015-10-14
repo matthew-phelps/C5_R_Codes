065f4d34cb14b8d71323d725649319353685cb05
cctamason <cctamason@gmail.com>
2015-10-14 08:49:42 -0400

    new plots for clothes and dish washing

--- W2_water_viz 2.R
+++ W2_water_viz 2.R
@@ -6,7 +6,7 @@
 rm(list = ls())
 graphics.off()
 ifelse(grepl("zrc340", getwd()), 
-       water.usage.path<- "C:\\Users\\zrc340\\Desktop\\Dropbox\\Cholera PhD\\5C\\Analysis\\C5_R_Codes\\Rdata\\monthly-water.Rdata",
+       water.usage.path<- "C:\\Users\\zrc340\\Desktop\\C5 for Git\\C5_R_Codes\\Rdata\\clean-monthly-baseline.Rdata",
        water.usage.path<- "C:\\Users\\wrz741\\Dropbox\\C5_R_Codes\\Rdata\\monthly-water.Rdata")
 
 library(ggplot2)
@@ -30,18 +30,19 @@ boxplot(daily_h2o_percapita ~ month, data = monthly[monthly$water_access_group =
 
 
 # Plot a subset of unique ids
-sub<-monthly[monthly$daily_h2o_percapita<250&monthly$daily_h2o_percapita>0,] #get rid of outliers 
-sub <- sub[sub$slno.1>100&sub$slno.1<250,]# assuming random assingment of numbers (which it was; there was no logical system to this)
+
+sub <- m4[m4$slno.1>100&m4$slno.1<200,]# assuming random assingment of numbers (which it was; there was no logical system to this)
 
 
 #plotting based on month doesn't work because we started in 2014. Create year.month variable to get dates in the correct order
-sub$month<-formatC(sub$month,width=2,format='d', flag = 0)
+
+
 sub$year.month<-as.numeric(with(sub, ifelse(month=="09"|month=="10"|month=="11"|month=="12",paste("14.",month,sep=""),paste("15.",month,sep=""))))
 sub<-sub[order(sub$year.month),]
 
 head(sub$year.month)
 nuniqueid<-max(sub$slno.1) #create number of lines, slno is a unique number created during baseline phase to merge baselines and is numeric
-xrange<-range(sub$year.month)
+xrange<-range(sub$date_visit)
 yrange<-range(sub$daily_h2o_percapita)
 
 plot(daily_h2o_percapita~date_visit,data = sub, xlab="Month",ylab="Daily water consumption per capita" )
@@ -53,4 +54,7 @@ plot(daily_h2o_percapita~date_visit,data = sub, xlab="Month",ylab="Daily water c
 ggplot(data = sub, aes(x = date_visit, y = daily_h2o_percapita)) +
   geom_line(aes( color = uniqueID))
 
+ggplot(data=sub, aes(x=date_visit, y= clothes)) + geom_line(aes( color = uniqueID))
+
+ggplot(data=sub, aes(x=date_visit, y= dishes)) + geom_line(aes( color = uniqueID))
 
